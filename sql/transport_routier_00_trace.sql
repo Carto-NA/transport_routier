ed0682e3475cfbff9d7afd802a8cd8c4606dd0ec
Carto-NA <63336882+Carto-NA@users.noreply.github.com>
2020-06-24 16:47:23 +0200

    Create transport_routier_00_trace.sql

---
+++ sql/transport_routier_00_trace.sql
@@ -0,0 +1,13 @@
+/*TRANSPORT_ROUTIER V1.0*/
+/*Creation du fichier "00_trace" qui permet de suivre l'évolution du code*/
+/* transport_routier_00_trace.sql */
+/*PostGIS*/
+
+/* Propriétaire : Conseil régional Nouvelle-Aquitaine - http://cartographie.nouvelle-aquitaine.fr/ */
+/* Auteur : Tony VINCENT*/
+
+
+/*
+#################################################################### SUIVI CODE SQL ####################################################################
+2020-06-24 : TV / initialisation du code uniquement
+2020-06-24 : TV / initialisation de la structure de la base de données sur la méthanisation
